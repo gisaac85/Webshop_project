2e6f75081feb3afda871b4b8dc23618344618a2b
Gaorieh Isaac <35237908+gisaac85@users.noreply.github.com>
2021-02-17 13:58:46 +0100

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Core.Entities;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IGenericRepository<Product> _productsRepo;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepo;
+        private readonly IGenericRepository<ProductType> _productTypeRepo;
+
+        public ProductsController(IGenericRepository<Product> productsRepo, IGenericRepository<ProductBrand> productBrandRepo, IGenericRepository<ProductType> productTypeRepo)
+        {           
+            _productTypeRepo = productTypeRepo;
+            _productBrandRepo = productBrandRepo;
+            _productsRepo = productsRepo;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {          
+            var products = await _productsRepo.ListAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _productsRepo.GetByIdAsync(id);
+            return Ok(product);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _productBrandRepo.ListAllAsync());
+        }
+        
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _productsRepo.ListAllAsync());
+        }
+    }
+}
---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseClass
+    {
+         Task<T> GetByIdAsync(int id);
+         Task<IReadOnlyList<T>> ListAllAsync();        
+        
+    }
+}
